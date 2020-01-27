22a78db6784e6133dccfdb881bf4bf029cde75f2
ThaisMou <35661836+ThaisMou@users.noreply.github.com>
2020-01-27 09:13:20 -0300

    add ":ApiController" in CineController

--- Challenge/Session3/src/CineApi/Controllers/CineCotroller.cs
+++ Challenge/Session3/src/CineApi/Controllers/CineCotroller.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CineApi.Controllers
+namespace CineApi.Controllers : ApiController
 {
     [Produces("application/json")]
     [Route("api/Usuarios")]
