5690bcf85ae6c57d57321b4e4c6f3f1b1d5b33bc
ThaisMou <35661836+ThaisMou@users.noreply.github.com>
2020-01-28 12:56:11 -0300

    Id from Movies updated

--- Challenge/Session3/CineFlix/Services/MovieRepository.cs
+++ Challenge/Session3/CineFlix/Services/MovieRepository.cs
@@ -14,15 +14,15 @@ namespace CineFlix.Services
                 return new Movie[]
                 {
                 new Movie { Id = 1, Title = "ASSASSIN'S CREED: EMBERS", Category = "Animation"},
-                new Movie { Id = 1, Title = "Real Steel(2012)", Category = "Animation"},
-                new Movie { Id = 1, Title = "Alvin and the Chipmunks", Category = "Animation"},
-                new Movie { Id = 1, Title = "The Adventures of Tin Tin", Category = "Animation"},
-                new Movie { Id = 1, Title = "Safe (2012)", Category = "Action"},
-                new Movie { Id = 1, Title = "Safe House(2012)", Category = "Action"},
-                new Movie { Id = 1, Title = "GIA", Category = "18+"},
-                new Movie { Id = 1, Title = "Deadline 2009", Category = "18+"},
-                new Movie { Id = 1, Title = "The Dirty Picture", Category = "18+"},
-                new Movie { Id = 1, Title = "Marley and me", Category = "Romance"}
+                new Movie { Id = 2, Title = "Real Steel(2012)", Category = "Animation"},
+                new Movie { Id = 3, Title = "Alvin and the Chipmunks", Category = "Animation"},
+                new Movie { Id = 4, Title = "The Adventures of Tin Tin", Category = "Animation"},
+                new Movie { Id = 5, Title = "Safe (2012)", Category = "Action"},
+                new Movie { Id = 6, Title = "Safe House(2012)", Category = "Action"},
+                new Movie { Id = 7, Title = "GIA", Category = "18+"},
+                new Movie { Id = 8, Title = "Deadline 2009", Category = "18+"},
+                new Movie { Id = 9, Title = "The Dirty Picture", Category = "18+"},
+                new Movie { Id = 10, Title = "Marley and me", Category = "Romance"}
                 };
         }
 
